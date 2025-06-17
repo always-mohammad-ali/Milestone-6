58dad12fc41e1e3da898a0ac6f82eca69514754d
Mohammad Ali <always.mohammad.ali@gmail.com>
2025-06-18 00:24:42 +0600

    lets make myself the best

--- Module-30/arrow-function.js
+++ Module-30/arrow-function.js
@@ -1,4 +1,17 @@
 const getAge = (person) => person.age;
+const getName = (person) => person.name;
 const student = {name:'ali', age:25};
 const studentAge = getAge(student);
 console.log(studentAge)
+
+const studentName = getName(student);
+console.log(studentName);
+
+const getRoll = (roll) => roll[3];
+const rollNo = getRoll([1,2,3,4,5,8,]);
+console.log(rollNo)
+
+const addNo = (num1, num2) => (num1 + num2)
+
+const total = addNo(5, 45);
+console.log(total);
