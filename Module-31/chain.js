2d898b1f64dbfe9736658f4b8dece653ed166260
Mohammad Ali <always.mohammad.ali@gmail.com>
2025-06-30 12:16:12 +0600

    chain of object, array

---
+++ Module-31/chain.js
@@ -0,0 +1,12 @@
+const subject = {
+    math:'howard precalc',
+    edition:5,
+    writer:{
+        writer1:'anton',
+        writer2:'bilbin'
+    },
+    years:[1923, 2010, 2001]
+}
+
+console.log(subject.writer?.writer1);
+console.log(subject.years[0]);
